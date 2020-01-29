744b3eebcba156986fe31821c63219a509503545
diharaw <shredhead94@gmail.com>
2020-01-29 00:34:27 +0000

    Clang formatted source.

--- include/demo_player.h
+++ include/demo_player.h
@@ -18,17 +18,17 @@ struct CubicSpline
     std::vector<float>       m_lengths;
 
     // Spline construction, Burden & Faires - Numerical Analysis 9th, algorithm 3.4
-    void initialize();
+    void      initialize();
     void      add_point(glm::vec3 p);
     glm::vec3 spline_at_time(float t);
-    float arc_length_integrand_single_spline(int spline, float t);
-    float arc_length_integrand(float t);
+    float     arc_length_integrand_single_spline(int spline, float t);
+    float     arc_length_integrand(float t);
     // Composite Simpson's Rule, Burden & Faires - Numerical Analysis 9th, algorithm 4.1
-    float simpsons_rule_single_spline(int spline, float t);
-    float simpsons_rule(float t0, float t1);
+    float     simpsons_rule_single_spline(int spline, float t);
+    float     simpsons_rule(float t0, float t1);
     glm::vec3 const_velocity_spline_at_time(float t, float speed);
-    float current_distance(float t, float speed);
-    float total_length();
+    float     current_distance(float t, float speed);
+    float     total_length();
 };
 
 class LerpSpline
@@ -58,12 +58,12 @@ private:
 public:
     DemoPlayer();
     ~DemoPlayer();
-    void edit_ui(Camera* camera);
-    void debug_visualization(DebugDraw& debug_draw);
-    float speed();
-    void  play();
-    void stop();
-    bool is_playing();
+    void      edit_ui(Camera* camera);
+    void      debug_visualization(DebugDraw& debug_draw);
+    float     speed();
+    void      play();
+    void      stop();
+    bool      is_playing();
     void      update(float dt, Camera* camera);
     glm::vec3 position();
     glm::vec3 forward();
